ac03b740d4b6570cba505b1ecfd64ee741a3b288
djsounddog <djsounddog@gmail.com>
2018-08-31 16:17:08 +1000

    Added onClick functionality to show sub views.

--- app/src/main/java/com/example/android/miwok/Numbers.java
+++ app/src/main/java/com/example/android/miwok/Numbers.java
@@ -2,6 +2,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Numbers extends AppCompatActivity {
 
@@ -9,5 +10,23 @@ public class Numbers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Create an array of words
+        String[] words = new String[10];
+        words[0] = "one";
+        words[1] = "two";
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[9] = "ten";
+
+        //Verify the contents of the array
+        for (int i=0; i<10; i++) {
+            Log.v("Word at index " + i + ": ", words[i]);
+        }
     }
 }
