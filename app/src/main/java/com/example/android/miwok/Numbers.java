35ae26d7c3d6fd7d8939e5b8ff23878f7480f815
djsounddog <djsounddog@gmail.com>
2018-09-03 19:27:50 +1000

    Added onClick functionality to show sub views.

--- app/src/main/java/com/example/android/miwok/Numbers.java
+++ app/src/main/java/com/example/android/miwok/Numbers.java
@@ -2,7 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -27,15 +29,15 @@ public class Numbers extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
         words.add("eleven");
+        words.add("twelve");
+        words.add("thirteen");
+        words.add("fourteen");
 
-        LinearLayout rootView = findViewById(R.id.rootView);
-        for (int i=0; i<words.size(); i++) {
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(i));
-            rootView.addView(wordView);
-        }
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>
+                (this, android.R.layout.simple_list_item_1, words);
 
+        ListView listView = findViewById(R.id.list);
 
-        //Verify the contents of the array
+        listView.setAdapter(itemsAdapter);
     }
 }
