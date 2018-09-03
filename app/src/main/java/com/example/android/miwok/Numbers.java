aaa67b68825418fe4f8a9e506043ca84ba2d7e6c
djsounddog <djsounddog@gmail.com>
2018-09-03 19:11:34 +1000

    Added onClick functionality to show sub views.

--- app/src/main/java/com/example/android/miwok/Numbers.java
+++ app/src/main/java/com/example/android/miwok/Numbers.java
@@ -2,7 +2,10 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Numbers extends AppCompatActivity {
 
@@ -12,21 +15,27 @@ public class Numbers extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
         //Create an array of words
-        String[] words = new String[10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+        ArrayList<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+        words.add("eleven");
+
+        LinearLayout rootView = findViewById(R.id.rootView);
+        for (int i=0; i<words.size(); i++) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(i));
+            rootView.addView(wordView);
+        }
+
 
         //Verify the contents of the array
-        for (int i=0; i<10; i++) {
-            Log.v("Word at index " + i + ": ", words[i]);
-        }
     }
 }
