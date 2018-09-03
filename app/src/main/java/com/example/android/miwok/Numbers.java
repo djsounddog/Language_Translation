b84f534071cfb00cb00f4113aafbb6052a6b6bdd
djsounddog <djsounddog@gmail.com>
2018-09-03 19:28:08 +1000

    Added onClick functionality to show sub views.

--- app/src/main/java/com/example/android/miwok/Numbers.java
+++ app/src/main/java/com/example/android/miwok/Numbers.java
@@ -3,9 +3,7 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
